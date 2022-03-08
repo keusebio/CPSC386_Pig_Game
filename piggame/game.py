4f554c74569e4e7903139665228a9aa88d40d2f6
keusebio <78199007+keusebio@users.noreply.github.com>
2022-03-08 15:44:09 -0800

    Complete game, missing rules

--- piggame/game.py
+++ piggame/game.py
@@ -1,5 +1,6 @@
 import time
 from .dice import C_Die
+from .player import C_AIPigPlayer
 from .player import C_Player
 class PigGame:
     def __init__(self):
@@ -9,39 +10,52 @@ class PigGame:
         """ Init game """
         d = C_Die()
         r = None
+        winFlag = False
+        AIFlag = False
+
+
+        """ get players """ 
         num_players = int(input('How Many Players? '))
         list_players = [None] * num_players
-        
-        """ get player names """        
+        if num_players == 1:
+            AIFlag = True
+      
         for p in list_players:
             i = list_players.index(p)
             p = input('Player {} name: '.format(i+1))
             list_players[i] = C_Player(p)
             
-        print(list_players)
+        if AIFlag == True:
+            list_players.append(C_AIPigPlayer())
         
         """ the GaMe """
-        while(True):
-            """ Game Start """
+        while(winFlag == False):
+            """ Welcome + rules """
+            
+            """ Game """
             for p in list_players:
                 total = p.score
                 print('It is {}\'s turn! Your current score is: {}'.format(p, p.score))
                 while(True):
-                    decision = input('{}, would you like to roll? (y/n) Current score: {} '.format(p, p.score))
-                    if decision == 'y':
+                    decision = p.does_roll() 
+                    
+                    if decision:
                         r = d.roll()
                         print('{} rolled a: {}'.format(p, r ))
                         p.score = r + p.score
+                        print('current score is: {}'.format(p.score))
                     else:
                         print('{} is holding'.format(p))
                         break
                     if r == 1:
                         p.score = 0
-                        print('You rolled a {}! Score reset back to 0, next player\'s turn.')
+                        print('{} rolled a 1! Score reset back to 0, next player\'s turn.'.format(p))
+                        break
+                    if p.score >= 30:
+                        winFlag = True
+                        break
                 
-                """
-                print("I'm in a game!")
-                rolled_number = d.roll()
-                print('You rolled: {}'.format(rolled_number))
-                time.sleep(2)
-                """
+                if winFlag == True:
+                    print('{} won with {} points! GG'.format(p, p.score))
+                    break
+                
